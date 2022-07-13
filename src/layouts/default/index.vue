827cc52219dbf1114a3e853ff0a7d9edca38e224
zhongyuan <zhongyuan9120@163.com>
2022-07-13 22:49:33 +0800

    improvement(index): 添加layout

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,3 +1,81 @@
 <template>
-  <div>layout</div>
+  <Layout>
+    <LayoutSider
+      class="bg-[#02C160]"
+      style="
+         {
+          min-width: 40px;
+        }
+      "
+      v-model:collapsed="collapsed"
+      :trigger="null"
+      collapsible
+    >
+      <div class="logo"></div>
+
+      <Menu
+        v-model:selectedKeys="selectedKeys"
+        theme="light"
+        mode="inline"
+        class="bg-[#02C160]"
+      >
+        <MenuItem name="ss" key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </MenuItem>
+        <MenuItem key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </MenuItem>
+        <MenuItem key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </MenuItem>
+      </Menu>
+    </LayoutSider>
+    <Layout>
+      <LayoutHeader style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </LayoutHeader>
+      <LayoutContent
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        <router-view style="height: 820px"></router-view>
+      </LayoutContent>
+    </Layout>
+  </Layout>
 </template>
+<script lang="ts" setup>
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue'
+import {
+  Layout,
+  LayoutSider,
+  Menu,
+  MenuItem,
+  LayoutHeader,
+  LayoutContent,
+} from 'ant-design-vue'
+import { ref } from 'vue'
+const selectedKeys = ref<string[]>(['1'])
+const collapsed = ref<boolean>(true)
+</script>
--- src/routers/routes/modules/task.ts
+++ src/routers/routes/modules/task.ts
@@ -1,14 +1,26 @@
 import { RoleEnum } from '@/enums/role-enum'
+import { LAYOUT } from '@/routers/constant'
 import { AppRouteRecordRaw } from '@/routers/types'
 
 export const Task: AppRouteRecordRaw = {
   path: '/task',
   name: 'task',
-  component: () => import('@/views/task/task.vue'),
+  component: LAYOUT, // () => import('@/views/task/task.vue'),
   meta: {
-    title: '任务',
+    title: '首页',
     roles: [RoleEnum.SUPER, RoleEnum.TEST],
   },
+  children: [
+    {
+      path: '',
+      name: 'task',
+      component: () => import('@/views/task/task.vue'),
+      meta: {
+        title: '任务列表',
+        roles: [RoleEnum.SUPER, RoleEnum.TEST],
+      },
+    },
+  ],
 }
 
 export default Task
